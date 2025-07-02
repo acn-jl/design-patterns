1bb9fd08dfc1997fb1719f229c36ea30f156901f
Laeno <robert.jesry.p.laeno@accenture.com>
2025-07-02 11:08:27 +0800

    001: add singleton

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
 // src/app.ts
-const message: string = "Hello, TypeScript!";
-console.log(message);
+
+import { Singleton } from "./creational-patterns/singleton";
+
+const run = (pattern: string) => {
+    switch(pattern) {
+        case "singleton":
+            const instance1 = Singleton.getInstance();
+            const instance2 = Singleton.getInstance();
+            console.log("Are both instances the same?", instance1 === instance2); // true
+            break;
+        case "factory":
+            break;
+        default:
+            console.log(`Unknown pattern: ${pattern}`);
+            break;
+    }
+};
+
+run("singleton1");
