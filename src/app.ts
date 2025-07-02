1519619e78045768cb629d9b47dfb0984fa54e45
Laeno <robert.jesry.p.laeno@accenture.com>
2025-07-02 11:14:30 +0800

    002: add factory

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 // src/app.ts
 
 import { Singleton } from "./creational-patterns/singleton";
+import { CarFactory } from "./creational-patterns/factory";
+
 
 const run = (pattern: string) => {
     switch(pattern) {
@@ -9,12 +11,20 @@ const run = (pattern: string) => {
             const instance2 = Singleton.getInstance();
             console.log("Are both instances the same?", instance1 === instance2); // true
             break;
+
         case "factory":
+            const car1 = CarFactory.createCar("Sedan");
+            car1.drive();  // Driving a sedan.
+
+            const car2 = CarFactory.createCar("SUV");
+            car2.drive();  // Driving an SUV.
             break;
+
         default:
             console.log(`Unknown pattern: ${pattern}`);
             break;
     }
 };
 
-run("singleton1");
+run("singleton");
+run("factory");
